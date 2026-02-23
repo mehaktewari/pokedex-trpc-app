60c6e4c67e9c98030b8c7315e46ff609ac0035e7
mehaktewari <131230377+mehaktewari@users.noreply.github.com>
2026-02-23 23:26:08 +0530

    Implemented pokemon tRPC router with filtering logic

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
-
+import { pokemonRouter } from "./routers/pokemon";
 /**
  * This is the primary router for your server.
  *
@@ -8,6 +8,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  pokemon: pokemonRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/pokemon.ts
@@ -0,0 +1,42 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const pokemonRouter = createTRPCRouter({
+
+  getPokemonByName: publicProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      return ctx.prisma.pokemon.findFirst({
+        where: {
+          name: {
+            equals: input,
+            mode: "insensitive",
+          },
+        },
+      });
+    }),
+
+  getMultiplePokemon: publicProcedure
+    .input(z.array(z.string()))
+    .query(async ({ ctx, input }) => {
+      return ctx.prisma.pokemon.findMany({
+        where: {
+          name: {
+            in: input,
+          },
+        },
+      });
+    }),
+
+  getPokemonByType: publicProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      return ctx.prisma.pokemon.findMany({
+        where: {
+          types: {
+            has: input,
+          },
+        },
+      });
+    }),
+});
