491293fcbf4ac4fa94a1b73e21af2543f128b134
mehaktewari <131230377+mehaktewari@users.noreply.github.com>
2026-02-24 01:32:26 +0530

    Final cleanup and completed assignment requirements

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter } from "~/server/api/trpc";
 import { pokemonRouter } from "./routers/pokemon";
 /**
  * This is the primary router for your server.
@@ -7,18 +6,9 @@ import { pokemonRouter } from "./routers/pokemon";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   pokemon: pokemonRouter,
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
 
-/**
- * Create a server-side caller for the tRPC API.
- * @example
- * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
- *       ^? Post[]
- */
-export const createCaller = createCallerFactory(appRouter);
