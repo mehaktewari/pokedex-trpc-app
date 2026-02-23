329fd974e04dab8f875eea3d6990bfe79583177f
mehaktewari <131230377+mehaktewari@users.noreply.github.com>
2026-02-24 00:56:10 +0530

    Integrated FilterablePokedexTable in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,41 +6,82 @@ import {
   CircularProgress,
   Alert,
   Typography,
+  Divider,
 } from "@mui/material";
+import { PokedexTable } from "../components/PokedexTable";
+import { FilterablePokedexTable } from "../components/FilterablePokedexTable";
 
 export default function Home() {
+  // Single search
   const [search, setSearch] = useState("");
 
   const {
-    data,
+    data: singlePokemon,
     isLoading,
     error,
   } = api.pokemon.getPokemonByName.useQuery(search, {
     enabled: !!search,
   });
 
+  // Multiple search
+  const [multipleInput, setMultipleInput] = useState("");
+
+  const { data: multiplePokemon } =
+    api.pokemon.getMultiplePokemon.useQuery(
+      multipleInput
+        .split(",")
+        .map((name) => name.trim())
+        .filter(Boolean),
+      { enabled: !!multipleInput }
+    );
+
   return (
     <Container sx={{ marginTop: 4 }}>
-      <h1>Pokedex Explorer</h1>
+      <Typography variant="h4" gutterBottom>
+        Pokedex Explorer
+      </Typography>
 
+      {/* ---------- Single Search ---------- */}
       <TextField
-        label="Search Pokemon"
-        variant="outlined"
+        label="Search Single Pokemon"
         fullWidth
         onChange={(e) => setSearch(e.target.value)}
+        sx={{ marginBottom: 2 }}
       />
 
       {isLoading && <CircularProgress />}
       {error && <Alert severity="error">Something went wrong</Alert>}
 
-      {data && (
+      {singlePokemon && (
         <div style={{ marginTop: 20 }}>
-          <Typography>ID: {data.id}</Typography>
-          <Typography>Name: {data.name}</Typography>
-          <Typography>Types: {data.types}</Typography>
-          <img src={data.sprite} width={120} />
+          <Typography>ID: {singlePokemon.id}</Typography>
+          <Typography sx={{ textTransform: "capitalize" }}>
+            Name: {singlePokemon.name}
+          </Typography>
+          <Typography sx={{ textTransform: "capitalize" }}>
+            Types: {singlePokemon.types}
+          </Typography>
+          <img src={singlePokemon.sprite} width={120} />
         </div>
       )}
+
+      <Divider sx={{ marginY: 4 }} />
+
+      {/* ---------- Multiple Search ---------- */}
+      <TextField
+        label="Enter multiple names (comma separated)"
+        fullWidth
+        onChange={(e) => setMultipleInput(e.target.value)}
+      />
+
+      {multiplePokemon && (
+        <PokedexTable pokemonList={multiplePokemon} />
+      )}
+
+      <Divider sx={{ marginY: 4 }} />
+
+      {/* ---------- Filter Section ---------- */}
+      <FilterablePokedexTable />
     </Container>
   );
 }
